9a0b23ee718a54b1b3baa7c3a4c74fe4bb84e83e
Nick Genovese <ngenovese@pepperdash.com>
2024-01-11 17:22:26 -0500

    feat: got the solution setup and cleaned up

---
+++ src/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       				// For Basic SIMPL#Pro classes
+
+namespace PDT.Plugins.Shure.DSP
+{
+    public class Class1
+    {
+    }
+}
+
